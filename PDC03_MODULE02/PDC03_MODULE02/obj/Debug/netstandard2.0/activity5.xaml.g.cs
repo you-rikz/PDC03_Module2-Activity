95e6a19e8a8cae3d5c3fcce263ef485a99e23108
Arnold Lim <arnoldlim0337@gmail.com>
2023-03-03 10:39:11 +0800

    Very Updated

--- PDC03_MODULE02/PDC03_MODULE02/obj/Debug/netstandard2.0/activity5.xaml.g.cs
+++ PDC03_MODULE02/PDC03_MODULE02/obj/Debug/netstandard2.0/activity5.xaml.g.cs
@@ -20,9 +20,13 @@ namespace PDC03_MODULE02 {
         private global::Xamarin.Forms.ListView lst;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ListView lstt;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(activity5));
             lst = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "lst");
+            lstt = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "lstt");
         }
     }
 }
